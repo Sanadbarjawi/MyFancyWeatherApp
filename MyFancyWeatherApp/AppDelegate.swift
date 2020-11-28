67f831886654ea4f5b86ccc978ddf976677df8c4
Sanad.b <sanad.amal.app@gmail.com>
2020-11-28 21:27:36 +0200

    storyboard approach, main storyboard with 2 viewControllers

--- MyFancyWeatherApp/AppDelegate.swift
+++ MyFancyWeatherApp/AppDelegate.swift
@@ -9,20 +9,11 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        window = UIWindow(frame: UIScreen.main.bounds)
-
-        let tabbarController = TabbarController()
-        window?.rootViewController = UINavigationController(
-            rootViewController: tabbarController
-        )
-
-        window?.makeKeyAndVisible()
         return true
     }
 
--- MyFancyWeatherApp/Modules/WeatherToday/WeatherTodayController.swift
+++ MyFancyWeatherApp/Modules/WeatherToday/WeatherTodayController.swift
@@ -7,73 +7,63 @@
 
 import UIKit
 
-struct TableSection: Hashable {
-    var title: String
-    var items: [TableItem]
-}
+final class WeatherTodayController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
 
-struct TableItem: Hashable {
-    var id: Int
-    var image: UIImage
-    var title: String
-    var color: UIColor
-}
-
-enum Data {
-    static var sections: [TableSection] = []
-//        [.init(title: "section1",
-//               items: [.init(id: 0, image: UIImage(named: "o1")!, title: "item1", color: .systemRed)]),
-//         .init(title: "section1",
-//               items: [.init(id: 1, image: UIImage(named: "o2")!, title: "item2", color: .systemBlue)]),
-//         .init(title: "section1",
-//               items: [.init(id: 2, image: UIImage(named: "o3")!, title: "item3", color: .systemPink)]),
-//         .init(title: "section1",
-//               items: [.init(id: 3, image: UIImage(named: "o4")!, title: "item4", color: .systemTeal)]),
-//         .init(title: "section1",
-//               items: [.init(id: 4, image: UIImage(named: "o5")!, title: "item5", color: .systemGray)])]
-}
-
-
-class WeatherTodayController: UIViewController {
-
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: view.bounds, style: .plain)
-        return tableView
-    }()
-
-    private var dataSource: UITableViewDiffableDataSource<TableSection, TableItem>?
+    @IBOutlet private weak var citiesPickerView: UIPickerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(tableView)
-        configureDataSource()
-        loadData()
-
-    }
-    
-    private func configureDataSource() {
-        dataSource = UITableViewDiffableDataSource<TableSection, TableItem>(
-            tableView: tableView,
-            cellProvider: { (_, _, item) -> UITableViewCell? in
-                let cell = UITableViewCell(style: .default, reuseIdentifier: "")
-//                cell.configure(item)
-                return cell
-            }
-        )
-        tableView.dataSource = dataSource
+        configureCitiesPickerView()
+        configureTableView()
     }
 
-    private func loadData() {
-
-        var snapShot = NSDiffableDataSourceSnapshot<TableSection, TableItem>()
-        snapShot.appendSections(Data.sections)
-
-        for section in Data.sections {
-            snapShot.appendItems(section.items, toSection: section)
-        }
-        dataSource?.apply(snapShot, animatingDifferences: true, completion: nil)
+    private func configureCitiesPickerView() {
+        citiesPickerView.delegate = self
+        citiesPickerView.dataSource = self
     }
 
+    private func configureTableView() {
+        tableView.dataSource = self
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.tableFooterView = UIView()
+    }
+}
+
+extension WeatherTodayController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "sss"
+    }
+
+}
+
+extension WeatherTodayController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("didSelect row")
+    }
+}
+
+extension WeatherTodayController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
 }
